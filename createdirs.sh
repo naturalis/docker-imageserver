eaaabae1bf438d9922aeef88cff645565c04f1b8
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2019-10-18 11:04:39 +0200

    added create folder script

---
+++ createdirs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Very simple create dir and asign permissions script for creating cacheing resize folders. 
+# run in image data folder where resize script and original folder exists. 
+#
+mkdir 1200x750
+mkdir 147x107
+mkdir 190x100
+mkdir 299x203
+mkdir 800x500
+mkdir comping
+mkdir 120x75
+mkdir 160x100
+mkdir 280x190
+mkdir 510x272
+mkdir 80x80
+chown www-data:www-data  1200x750
+chown www-data:www-data 147x107
+chown www-data:www-data 190x100
+chown www-data:www-data 299x203
+chown www-data:www-data 800x500
+chown www-data:www-data comping
+chown www-data:www-data 120x75
+chown www-data:www-data 160x100
+chown www-data:www-data 280x190
+chown www-data:www-data 510x272
+chown www-data:www-data 80x80
+
