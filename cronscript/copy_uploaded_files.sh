4515998cf533c09ea148cb809de3c36f015fa494
maarten.schermer@naturalis.nl <maarten.schermer@naturalis.nl>
2019-10-21 10:43:18 +0200

    added cronjob

--- cronscript/copy_uploaded_files.sh
+++ cronscript/copy_uploaded_files.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-INCOMING=/home/imageupload/incoming/
-IMAGE_ROOT=/data/www/
-TARGET=/data/www/original/
+INCOMING=/home/imageupload/incoming
+IMAGE_ROOT=/data/www
+TARGET=/data/www/original
 NOW=$( date +"%Y-%m-%d %H:%M:%S" )
+
+# secondary incoming directory is fed by minio file browser
+SECONDARY_INCOMING=/data/incoming/images
+MINUTES=$(date "+%M")
+MINUTES_LAST_DIGIT="${MINUTES:1:1}"
+
+if [ "$MINUTES_LAST_DIGIT" == "7" ]; then
+  # non-empty secondary incoming directory, move files to primary
+  if [ "$(ls -A $SECONDARY_INCOMING)" ]; then
+    mv $SECONDARY_INCOMING/* $INCOMING/
+  fi
+fi
+
 
 # empty incoming directory, silent exit
 if [ ! "$(ls -A $INCOMING)" ]; then
@@ -12,14 +25,14 @@ fi
 
 echo "start $NOW"
 
-if [[ -n $(find $INCOMING -name "*.filepart") ]]; then
+if [[ -n $(find $INCOMING/ -name "*.filepart") ]]; then
   echo "found '.filepart' in $INCOMING, file transfer in progress; exiting."
   exit
 fi
 
 
 echo "cleaning up before moving"
-find $INCOMING -type f | while read f; do
+find $INCOMING/ -type f | while read f; do
   if [[ -f $f ]]; then
     # file
     if [[ ! ${f,,} == *.jpg && ! ${f,,} == *.png ]]; then
@@ -43,11 +56,11 @@ fi
 
 
 echo "looking for existing resized copies of new files"
-for f in $INCOMING* $INCOMING**/* ; do
+for f in $INCOMING/* $INCOMING/**/* ; do
   if [[ -f $f ]]; then
     filename=$(basename "$f")
     IFS=$'\n';
-    for g in $(find $IMAGE_ROOT -name $filename);
+    for g in $(find $IMAGE_ROOT/ -name $filename);
     do
       echo "found $g"
       if  [[ ! $g == *original/* ]] ;
@@ -63,8 +76,8 @@ done;
 
 
 echo "moving files to $TARGET"
-cp -r $INCOMING* $TARGET
-rm -R $INCOMING*
+cp -r $INCOMING/* $TARGET/
+rm -R $INCOMING/*
 
 echo "done"
 
